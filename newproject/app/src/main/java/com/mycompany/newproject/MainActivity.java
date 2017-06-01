a4b3caae65f17e9db8dfe216e347114d31356da2
Beoulve <beoulve87@gmail.com>
2017-06-01 17:05:17 +0300

    yes

--- newproject/app/src/main/java/com/mycompany/newproject/MainActivity.java
+++ newproject/app/src/main/java/com/mycompany/newproject/MainActivity.java
@@ -7,10 +7,16 @@ import android.view.View.*;
 
 public class MainActivity extends Activity implements OnClickListener
 {
-    final Button button1 = (Button)findViewById(R.id.button1);
-    final Button button2 = (Button)findViewById(R.id.button2);
-    final Button button3 = (Button)findViewById(R.id.button3);
-    final Button button4 = (Button)findViewById(R.id.button4);
+	//проверяем, какая кнопка нажата
+	@Override
+	public void onClick(View v){
+		switch (v.getId()){
+            case R.id.button1: Toast.makeText(this, "Нажата кнопка button1", Toast.LENGTH_SHORT).show(); break;
+			case R.id.button2: Toast.makeText(this, "Нажата кнопка button2", Toast.LENGTH_SHORT).show(); break;
+			case R.id.button3: Toast.makeText(this, "Нажата кнопка button3", Toast.LENGTH_SHORT).show(); break;
+			case R.id.button4: Toast.makeText(this, "Нажата кнопка button4", Toast.LENGTH_SHORT).show(); break;
+      }
+	}
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -20,21 +26,16 @@ public class MainActivity extends Activity implements OnClickListener
         TextView textView1 = (TextView) findViewById(R.id.textView1);
         textView1.setText ("Next text in TextView");
    
+		final Button button1 = (Button)findViewById(R.id.button1);
+		final Button button2 = (Button)findViewById(R.id.button2);
+		final Button button3 = (Button)findViewById(R.id.button3);
+		final Button button4 = (Button)findViewById(R.id.button4);
+		
     //один обработчки для всех кнопок
     button1.setOnClickListener(this);
     button2.setOnClickListener(this);
     button3.setOnClickListener(this);
     button4.setOnClickListener(this);
-    }
-
-    //проверяем, какая кнопка нажата
-    public void onClick(View v){
-        switch (v.getId()){
-            case R.id.button1: editText.setText("Нажата кнопка button1"); break;
-            case R.id.button2: editText.setText("Нажата кнопка button2"); break;
-            case R.id.button3: editText.setText("Нажата кнопка button3"); break;
-            case R.id.button4: editText.setText("Нажата кнопка button4"); break;
-        }
-    }
-
+  }
 }
+
