fe75807e39bff94595e610259004c656bb3543fb
Dmitriy <beoulve87@gmail.com>
2017-06-05 12:24:46 +0300

    Update MainActivity.java

--- newproject/app/src/main/java/com/mycompany/newproject/MainActivity.java
+++ newproject/app/src/main/java/com/mycompany/newproject/MainActivity.java
@@ -1,22 +1,19 @@
-package com.mycompany.newproject;
+package com.mycompany.myapp;
 import android.app.*;
 import android.os.*;
-import android.view.*;
-import android.widget.*;
-import android.view.View.*;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends Activity implements OnClickListener
+public class MainActivity extends Activity implements onClickListener
 {
-	//проверяем, какая кнопка нажата
-	@Override
-	public void onClick(View v){
-		switch (v.getId()){
-            case R.id.button1: Toast.makeText(this, "Нажата кнопка button1", Toast.LENGTH_SHORT).show(); break;
-			case R.id.button2: Toast.makeText(this, "Нажата кнопка button2", Toast.LENGTH_SHORT).show(); break;
-			case R.id.button3: Toast.makeText(this, "Нажата кнопка button3", Toast.LENGTH_SHORT).show(); break;
-			case R.id.button4: Toast.makeText(this, "Нажата кнопка button4", Toast.LENGTH_SHORT).show(); break;
-      }
-	}
+    final Button button1 = (Button)findViewById(R.id.button1);
+    final Button button2 = (Button)findViewById(R.id.button2);
+    final Button button3 = (Button)findViewById(R.id.button3);
+    final Button button4 = (Button)findViewById(R.id.button4);
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -25,17 +22,35 @@ public class MainActivity extends Activity implements OnClickListener
         setContentView(R.layout.main);
         TextView textView1 = (TextView) findViewById(R.id.textView1);
         textView1.setText ("Next text in TextView");
-   
-		final Button button1 = (Button)findViewById(R.id.button1);
-		final Button button2 = (Button)findViewById(R.id.button2);
-		final Button button3 = (Button)findViewById(R.id.button3);
-		final Button button4 = (Button)findViewById(R.id.button4);
-		
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setText ("My button");
+        button1.setEnabled (true);
+        CheckBox chb1 = (CheckBox) findViewById (R.id.chb1);
+        chb1.setChecked (true);
+
     //один обработчки для всех кнопок
     button1.setOnClickListener(this);
     button2.setOnClickListener(this);
     button3.setOnClickListener(this);
     button4.setOnClickListener(this);
-  }
-}
+    }
 
+    //новое действие
+    @Override
+    public void onMyClick(View v){
+        switch (v.getId()){
+            case R.id.button1: editText("тест нового метода"); break;
+        }
+    }
+    //проверяем, какая кнопка нажата.
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            //case R.id.button1: editText.setText("Нажата кнопка button 1"); break;
+            case R.id.button2: editText.setText("Нажата кнопка button 2"); break;
+            case R.id.button3: editText.setText("Нажата кнопка button 3"); break;
+            case R.id.button4: editText.setText("Нажата кнопка button 4"); break;
+        }
+    }
+
+}
